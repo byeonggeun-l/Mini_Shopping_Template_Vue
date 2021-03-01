9cac661c9ea529853757a0aed3c1a750e2b39673
bglim <bglim@ui-lab.co.kr>
2021-03-01 19:57:51 +0900

    New Build.
    New Build.

--- src/router.js
+++ src/router.js
@@ -1,21 +1,17 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from './views/Home';
-import RouterTest from './views/RouterTest';
-import Context_Menu_Original from './views/Components/01_Context_Menu/Context_Menu_Original';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "./views/Home";
+import RouterTest from "./views/RouterTest";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        // Default
-        { path: '/', component: Home },
-        { path: '/my_component', component: Home },
-        { path: '/routertest', component: RouterTest },
-        // Components
-        { path: '/context_menu_original', component: Context_Menu_Original },
-    ],
+  mode: "history",
+  routes: [
+    { path: "/", component: Home },
+    { path: "/Mini_Shopping_Template_Vue", component: Home },
+    { path: "/routertest", component: RouterTest },
+  ],
 });
 
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath:
-		process.env.NODE_ENV === 'production' ?'/my_component/' :'/',
+		process.env.NODE_ENV === 'production' ?'/Mini_Shopping_Template_Vue/' :'/',
     outputDir: 'docs',
 };
