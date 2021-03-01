8505d2a523ef201c63c11d1f5b7b8d53691bd306
bglim <bglim@ui-lab.co.kr>
2021-03-01 19:59:39 +0900

    New Build.
    New Build.

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "./views/Home";
-import RouterTest from "./views/RouterTest";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './views/Home';
+import RouterTest from './views/RouterTest';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: "history",
-  routes: [
-    { path: "/", component: Home },
-    { path: "/Mini_Shopping_Template_Vue", component: Home },
-    { path: "/routertest", component: RouterTest },
-  ],
+    mode: 'history',
+    routes: [
+        { path: '/', component: Home },
+        { path: '/Mini_Shopping_Template_Vue', component: Home },
+        { path: '/routertest', component: RouterTest },
+    ],
 });
 
 export default router;
